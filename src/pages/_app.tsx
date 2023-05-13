d20b9e58b57c24787e873d4bffc2ab165f0a6282
Omeiza Owuda <owudaomeiza@gmail.com>
2023-05-13 21:25:47 +0100

    Major start for daily scoop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.scss';
+import { IBM_Plex_Serif } from 'next/font/google';
 import type { AppProps } from 'next/app';
 
+const ibmPlexSerif = IBM_Plex_Serif({
+    weight: ['400', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    display: 'swap',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <style jsx global>{`html { font-family: ${ibmPlexSerif.style.fontFamily}; }`}</style>
+            <Component {...pageProps} />
+        </>
+    )
 }

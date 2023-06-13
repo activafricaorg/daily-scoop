fa33ae671b8da9d56d12935470966d30dfc3c8a6
Omeiza Owuda <owudaomeiza@gmail.com>
2023-06-13 16:18:17 +0100

    Update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,9 @@
 import headingStyles from "@/styles/Heading.module.scss";
 import utilityStyles from "@/styles/Utility.module.scss";
 import Image from "next/image";
+import Link from "next/link";
+import Category from "@/components/Category";
+import React from "react";
 
 interface LayoutProps {
 	children: string | JSX.Element | JSX.Element[]
@@ -10,13 +13,23 @@ const Layout = ({children} : LayoutProps) => {
 	return (
 		<main>
 			<header>
-				<Image src="/images/daily-scoop-logo.png" width={150} height={67} alt="Daily Scoop Africa" />
+				<Image className="logo" src="/images/daily-scoop-logo.png" width={150} height={67} alt="Daily Scoop Africa" />
+				<div className={headingStyles.menuTrigger}>MENU</div>
 			</header>
 			<section className={ headingStyles.container }>
 				<div className={`${utilityStyles.grid} ${utilityStyles.grid} ${utilityStyles.gridMain}`}>
 					{ children }
 				</div>
 			</section>
+			<nav className={headingStyles.mainMenu}>
+				<div className={headingStyles.mainMenuWrapper}>
+					<Category currentCategory={undefined} />
+					<ul className={headingStyles.secondaryMenu}>
+						<li className={headingStyles.mainMenuItem}><Link href="/about">About</Link></li>
+						<li className={headingStyles.mainMenuItem}><Link href="/contribute">Contribute</Link></li>
+					</ul>
+				</div>
+			</nav>
 		</main>
 	)
 }
